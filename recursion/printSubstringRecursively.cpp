553df13bb515a7f1d1572bd9a82f00a0626da74b
Bharatgupta922 <gbharat922@gmail.com>
2020-06-02 18:43:43 +0530

    updated

---
+++ recursion/printSubstringRecursively.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void printSub(string s, string curr = "", int index = 0)
+{
+    if (index == s.length())
+    {
+        cout << "'" << curr << "'"
+             << ","
+             << " ";
+        return;
+    }
+    printSub(s, curr, index + 1);
+    printSub(s, curr + s[index], index + 1);
+}
+int main()
+{
+    string s = "abc";
+    printSub(s);
+    return 0;
+}
