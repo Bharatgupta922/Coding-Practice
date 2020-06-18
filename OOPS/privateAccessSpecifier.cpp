270440a09286f9c51e6a816b1320b639eb55dcb6
Bharatgupta922 <gbharat922@gmail.com>
2020-06-18 17:23:16 +0530

    Learning how to use private access Specifiers for members and functions

--- OOPS/privateAccessSpecifier.cpp
+++ OOPS/privateAccessSpecifier.cpp
@@ -1,22 +1,31 @@
-#include<iostream>
-using namespace std ;
-class Man{
-    private:
-    int age ;
-
-    public:
-    void displayAge(){
-        cout<<age<<endl; // if i want to manipulate the age we can manipulate 
+#include <iostream>
+using namespace std;
+class Man
+{
+private:
+    int age;
+    int getAge()
+    {
+        return age - 5; // here we can manipulate the age
     }
-    void setAge(int value){
-        age = value - 5; // or here we can manipulate the given age 
+
+public:
+    void displayAge()
+    {
+        cout << getAge() << endl; // if i want to manipulate the age we can manipulate
+    }
+    void setAge(int value)
+    {
+        age = value - 5; // or here we can manipulate the given age
     }
 };
-int main(){
-Man bharat  ;
-//bharat.age = "Bharat Gupta";       // b/c i dont want to give access to the user to chang the age
-bharat.setAge(25);
-bharat.displayAge(); 
+int main()
+{
+    Man bharat;
+    //bharat.age = "Bharat Gupta";       // b/c i dont want to give access to the user to chang the age
+    //bharat.
+    bharat.setAge(25);
+    bharat.displayAge();
 
     return 0;
 }
