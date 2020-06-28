e5f18eaf94ddbc424f3af9f91962eaeceba2b3d9
Bharatgupta922 <gbharat922@gmail.com>
2020-06-28 17:04:43 +0530

    input and output comment added

--- Graph/KruskalsAlgorithm.cpp
+++ Graph/KruskalsAlgorithm.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
-//GREEDY ALGORITHM 
+//GREEDY ALGORITHM
 class Edge
 {
 public:
-// it is an undirected graph 
+    // it is an undirected graph
     int src;
     int dest;
     int weight;
@@ -29,7 +29,6 @@ void KrusKals(Edge *edge, int n, int E)
 {
     //sort the array on the basis of weight
     sort(edge, edge + E, compare);
-
 
     Edge *output = new Edge[n - 1];
     int *parent = new int[n];
@@ -64,7 +63,34 @@ void KrusKals(Edge *edge, int n, int E)
 
 int main()
 {
+    //example
 
+    //input graph with weight
+    //     9 14
+    // 0 1 4
+    // 0 7 8
+    // 1 2 8
+    // 1 7 11
+    // 7 8 7
+    // 7 6 1
+    // 2 8 2
+    // 8 6 6
+    // 2 3 7
+    // 6 5 2
+    // 2 5 4
+    // 3 5 14
+    // 3 4 9
+    // 4 5 10
+
+    //output
+    // 7 6 1
+    // 2 8 2
+    // 6 5 2
+    // 0 1 4
+    // 2 5 4
+    // 2 3 7
+    // 0 7 8
+    // 3 4 9
     int n, E;
     cin >> n >> E;
     Edge *edge = new Edge[E];
