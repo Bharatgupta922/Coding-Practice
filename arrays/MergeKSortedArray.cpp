e93f029d0ccbd11f854cc1439c0068d08c8b777f
Bharatgupta922 <gbharat922@gmail.com>
2020-07-04 20:32:50 +0530

    new change

---
+++ arrays/MergeKSortedArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#define N 105
+using namespace std;
+int *mergeKArrays(int arr[][N],int k);
+void printArray(int arr[], int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    int arr[N][N];
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	int *output = mergeKArrays(arr, k);
+	printArray(output, k*k);
+	cout<<endl;
+}
+	return 0;
+}
+
+
+// your task is tocomplete this function
+// function should return an pointer to output array int*
+// of size k*k
+int *mergeKArrays(int arr[][N], int n)
+{
+    int a [n*n];
+    return a;
+}
