b662076ad95163c738b208462ca6ad3d31f191a9
Bharatgupta922 <gbharat922@gmail.com>
2020-07-11 00:17:20 +0530

    Code for Rearranging the letters in a way that no two characters came adjacent

---
+++ strings/RearrangeTheString_noadjacentCharIsSame.cpp
@@ -0,0 +1,67 @@
+// Rearrange characters in a string such that no two adjacent are same
+// Given a string with repeated characters, the task is to rearrange characters
+// in a string so that no two adjacent characters are same.
+
+#include <bits/stdc++.h>
+using namespace std;
+//this class or structure makes the priority queue 
+//sort by second element of pair 
+struct myComp
+{
+    constexpr bool operator()(pair<int, int> const &a, pair<int, int> const &b)
+        const noexcept
+    {
+        return a.second < b.second;
+    }
+};
+void rearrange(string s)
+{
+    string op = "";
+    priority_queue<pair<char, int>, vector<pair<char, int>>, myComp> pq;
+    int freq[26] = {0};
+    for (int i = 0; i < s.length(); ++i)
+    {
+        ++freq[s[i] - 'a'];
+    }
+    for (int i = 0; i < 26; ++i)
+    {
+        if (freq[i] > 0)
+        {
+            pq.push(make_pair(i + 'a', freq[i]));
+        }
+    }
+    pair<char, int> lastvis;
+    lastvis.first = '#';
+    lastvis.second = -1;
+    while (!pq.empty())
+    {
+        pair<char, int> temp = pq.top();
+        pq.pop();
+        op.push_back(temp.first);
+        --temp.second;
+        if (lastvis.second > 0)
+        {
+            pq.push(lastvis);
+        }
+        lastvis = temp;
+    }
+    if (op.length() == s.length())
+    {
+        cout << op << endl;
+    }
+    else
+    {
+        cout << "it cannnot be reagganged !" << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        rearrange(s);
+    }
+}
