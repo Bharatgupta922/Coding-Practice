4a380893e7aa7e76cbd9e83c06e07864d57c1f62
Bharatgupta922 <gbharat922@gmail.com>
2020-07-17 23:45:46 +0530

    prims algorithm using adjacency matrix modified

--- Graph/PrimsAlgorithm.cpp
+++ Graph/PrimsAlgorithm.cpp
@@ -29,7 +29,7 @@ void prims(int **edges, int n)
     for (int i = 0; i < n - 1; ++i)
     {
 
-        int minVertex = findMinVertex(weight, visited, n);
+        int minVertex = findMinVertex(weight, visited, n);+
         visited[minVertex] = true;
         for (int j = 0; j < n; ++j)
         {
