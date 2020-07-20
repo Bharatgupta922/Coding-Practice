9bfef4f53752652f3595d0ca292bae76c8403190
Bharatgupta922 <gbharat922@gmail.com>
2020-07-20 18:16:02 +0530

    explaination code for private inheritance

---
+++ OOPS/Private_inheritance.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namesppace std;
+
+
+class Person
+{
+protected:
+    string name;
+
+public:
+    void setName(string iname)
+    {
+        name = iname;
+    }
+};
+
+class Student : private Person
+{
+
+    // all the public members of the Parent class will act as private members of this class
+    // all the protected members of the Parent class will act as private members of this class
+    // but the private members of Parent class is not accessible in this child / derived class
+
+public:
+    void display()
+    {
+        cout << name << endl;
+    }
+    void studentSetName(string iname)
+    {
+        setName(iname);
+    }
+};
+class GStudent : public Student
+{
+public:
+    void setGStudentName(string iname)
+    {
+        studentSetName(iname);
+    }
+};
+int main()
+{
+    GStudent bharat;
+    bharat.setGStudentName("Bharat Gupta");
+    bharat.display();
+
+    return 0;
+}
