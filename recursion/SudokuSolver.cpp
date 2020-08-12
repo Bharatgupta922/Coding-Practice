c9309ef31748b6fb6458aaae0984a246cac9fc20
Bharatgupta922 <gbharat922@gmail.com>
2020-08-12 14:15:30 +0530

    backtracking code for sudoku solving

--- recursion/SudokuSolver.cpp
+++ recursion/SudokuSolver.cpp
@@ -89,8 +89,8 @@ int main()
     int n;
     cin >> n;
     int board[D][D];
-    for(int i =0 ;i <n ; ++i){
-        for(int j = 0 ; j < n ; ++j){
+    for (int i =0;i <n; ++i) {
+        for (int j = 0; j < n; ++j) {
             cin>>board[i][j];
         }
     }
