5db9ff8c182b8d2390a0290f12317c0ddc606616
Bharatgupta922 <gbharat922@gmail.com>
2020-08-26 23:13:57 +0530

    modified equal partition sum problem

--- Dynamic_Programming/EqualPartitionSumProblem.cpp
+++ Dynamic_Programming/EqualPartitionSumProblem.cpp
@@ -1,4 +1,5 @@
-// Given a set of numbers, check whether it can be partitioned into two subsets such that the sum of elements in both subsets is same or not.
+// Given a set of numbers, check whether it can be partitioned into two subsets
+//such that the sum of elements in both subsets is same or not.
 
 // Input:
 // 2
