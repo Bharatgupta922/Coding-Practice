203196e7524af7eb59324bb02ac25458fc7e471f
Bharatgupta922 <gbharat922@gmail.com>
2020-08-26 23:18:07 +0530

    code to find out length Of Longest Palindromic Subsequence

---
+++ Dynamic_Programming/lengthOf_LongestPalindromicSubsequence.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int recursion(string x, string y, int m, int n)
+{
+    if (m == 0 || n == 0)
+    {
+        return 0;
+    }
+    if (x[m - 1] == y[n - 1])
+    {
+        return 1 + recursion(x, y, m - 1, n - 1);
+    }
+    else
+    {
+        return max(recursion(x, y, m - 1, n), recursion(x, y, m, n - 1));
+    }
+}
+int memoization(string x, string y, int m, int n, int **dp)
+{
+    if (m == 0 || n == 0)
+    {
+        return 0;
+    }
+    if (dp[m][n] != -1)
+    {
+        return dp[m][n];
+    }
+    if (x[m - 1] == y[n - 1])
+    {
+        return dp[m][n] = 1 + memoization(x, y, m - 1, n - 1, dp);
+    }
+    else
+    {
+        return dp[m][n] = max(memoization(x, y, m - 1, n, dp), memoization(x, y, m, n - 1, dp));
+    }
+}
+int tabulation(string x, string y, int m, int n)
+{
+    int dp[m + 1][n + 1];
+    for (int i = 0; i < m + 1; ++i)
+    {
+        for (int j = 0; j < n + 1; ++j)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 1; i < m + 1; ++i)
+    {
+        for (int j = 1; j < n + 1; ++j)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+void solve(string x, string y, int m, int n)
+{
+    int **dp = new int *[m + 1];
+    for (int i = 0; i < m + 1; ++i)
+    {
+        dp[i] = new int[n + 1];
+    }
+    for (int i = 0; i < m + 1; ++i)
+    {
+        for (int j = 0; j < n + 1; ++j)
+        {
+            dp[i][j] = -1;
+        }
+    }
+    //get lcs of both the strings
+    cout << tabulation(x, y, m, n) << endl;
+    // cout << memoization(x, y, m, n, dp) << endl;
+    // cout << recursion(x, y, m, n) << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m, n;
+        string x, temp;
+        cin >> m >> x;
+        temp = x;
+        reverse(temp.begin(), temp.end());
+        string y = temp;
+        // b/c length of reversed string = length of the string
+        solve(x, y, m, m);
+    }
+}
